f1b88100dca911e83fdd7ba110867a1ce8c4c181
Jomo Fisher <jomof@google.com>
2019-11-02 12:05:25 -0700

    more diagnostic

--- docker-build.sh
+++ docker-build.sh
@@ -5,11 +5,13 @@ docker pull $DOCKER_BASE
 docker build \
 --cache-from=$DOCKER_BASE:latest \
 -f ./DockerFiles/$TARGET/DockerFile \
+-t $DOCKER_BASE \
 -t $DOCKER_BASE:latest \
 -t $DOCKER_BASE:$BUILD_ID \
 -t $DOCKER_BASE:$SHORT_SHA \
 .
 
+docker push $DOCKER_BASE:latest
 docker push $DOCKER_BASE:latest
 docker push $DOCKER_BASE:$BUILD_ID
 docker push $DOCKER_BASE:$SHORT_SHA
