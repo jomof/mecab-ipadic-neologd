77e165da0eda921582ca40e3f59d69a21be7ecb8
Jomo Fisher <jomof@google.com>
2019-11-02 11:14:54 -0700

    more diagnostic

--- docker-build.sh
+++ docker-build.sh
@@ -8,6 +8,7 @@ docker build \
 -t $DOCKER_BASE:latest \
 -t $DOCKER_BASE:$BUILD_ID \
 -t $DOCKER_BASE:$SHORT_SHA \
+--mount src="/workspace",target=/workspace,type=bind k3_s3
 .
 
 docker push $DOCKER_BASE:latest
